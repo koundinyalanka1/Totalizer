ab79398dab02fd474aaa7b9da06585510b47d41c
koundinya <koundinya@koundinya-desktop>
2016-12-23 17:43:23 +0530

    This is a new commit.

---
+++ src/hacker/Hacker.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hacker;
+
+import my.Password.Password;
+
+/**
+ *
+ * @author revanthsivaji
+ */
+public class Hacker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Password obj=new Password();
+                obj.setVisible(true);
+                
+    }
+    
+}
